c5cee591fb0c9ab28c1109b3671a10c98746a263
Vladislav Li <k1541500@kingston.ac.uk>
2019-03-23 13:07:37 +0000

    Adding LightShaderGraph project to the repository including assets required to run the project.

---
+++ LightweightShaderGraph/Assets/Food/Scripts/MaterialSwitcher.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class MaterialSwitcher : MonoBehaviour {
+
+	[SerializeField]
+	private Rotator rotator;
+
+	[SerializeField]
+	private Material[] materials;
+
+	private void Reset() {
+
+		rotator = GetComponent<Rotator>();
+
+		// we need to assign them manualy;
+		// from the editor;
+		materials = new Material[3];
+	}
+
+	public void SwitchFresnel() {
+
+		Material material = null;
+
+		for (int i = 0; i < materials.Length; i++) {
+
+			if (materials[i].name.StartsWith("Food_Fresnel")) {
+
+				material = materials[i];
+			}
+		}
+
+		for (int i = 0; i < rotator.food.Length; i++) {
+
+			rotator.food[i].GetComponent<MeshRenderer>().material = material;
+		}
+	}
+
+	public void SwitchDissolve() {
+
+		Material material = null;
+
+		for (int i = 0; i < materials.Length; i++)
+		{
+
+			if (materials[i].name.StartsWith("Food_Dissolve")) {
+				material = materials[i];
+			}
+		}
+
+		for (int i = 0; i < rotator.food.Length; i++) {
+			rotator.food[i].GetComponent<MeshRenderer>().material = material;
+		}
+	}
+
+	public void SwitchHologram() {
+
+		Material material = null;
+
+		for (int i = 0; i < materials.Length; i++) {
+
+			if (materials[i].name.StartsWith("Food_Hologram")) {
+				material = materials[i];
+			}
+		}
+
+		for (int i = 0; i < rotator.food.Length; i++) {
+			rotator.food[i].GetComponent<MeshRenderer>().material = material;
+		}
+	}
+
+}
---
+++ LightweightShaderGraph/Assets/Food/Scripts/Toggler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Toggler : MonoBehaviour {
+
+	[SerializeField]
+	private Rotator rotator;
+
+	private void Reset() {
+
+		rotator = GetComponentInChildren<Rotator>();
+	}
+
+	public void Toggle(int foodIndex) {
+
+		for (int i = 0; i < rotator.food.Length; i++) {
+
+			// disable everything except chosen food;
+			if (i != foodIndex) {
+				rotator.food[i].gameObject.SetActive(false);
+			}
+
+		}
+
+		// because we toggle it instead;
+		rotator.food[foodIndex].gameObject.SetActive(!rotator.food[foodIndex].gameObject.activeSelf);
+	}
+
+}
